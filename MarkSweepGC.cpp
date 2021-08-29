78c16669c549e1095dd03d28d8c115873411f5fe
dimitroffangel <angeldimitrov05@gmail.com>
2021-08-29 15:42:00 +0300

    moved the MarkSweepGC header functions to cpp functions and removed adding the root before visiting it when collecting GC

--- MarkSweepGC.cpp
+++ MarkSweepGC.cpp
@@ -1,1 +1,69 @@
 #include "MarkSweepGC.h"
+
+MarkSweepGC::MarkSweepGC(const size_t gcInterval)
+	: m_GCInterval(gcInterval)
+{
+
+}
+
+void* MarkSweepGC::Allocate(const size_t size)
+{
+    if (++m_AllocsSinceGC == m_GCInterval)
+    {
+        CollectGarbage();
+        m_AllocsSinceGC = 0;
+    }
+
+    auto memory = ::operator new(size);
+    m_Allocated.insert(static_cast<Object*>(memory));
+    return memory;
+}
+
+void MarkSweepGC::CollectGarbage()
+{
+    m_Visited.clear();
+
+    // TODO:: what happens if  m_root == nullptr and m_AllocsSinceGC > 0 
+    if (m_Root == nullptr)
+    {
+        return;
+    }
+
+    (*m_Root)->VisitReferences(this, nullptr, m_Visited);
+
+    for (const auto object : m_Allocated)
+    {
+        if (m_Visited.find(object) == m_Visited.end())
+        {
+            object->~Object();
+            ::operator delete(object);
+        }
+    }
+
+    m_Allocated = m_Visited;
+}
+
+void MarkSweepGC::Shutdown()
+{
+    m_Visited.clear();
+
+    // TODO:: what happens if  m_root == nullptr and m_AllocsSinceGC > 0 
+    if (m_Root == nullptr)
+    {
+        return;
+    }
+
+    m_Visited.insert(*m_Root);
+    (*m_Root)->VisitReferences(this, nullptr, m_Visited);
+
+    for (const auto object : m_Allocated)
+    {
+        if (m_Visited.find(object) == m_Visited.end())
+        {
+            object->~Object();
+            ::operator delete(object);
+        }
+    }
+
+    m_Allocated = m_Visited;
+}
--- MarkSweepGC.h
+++ MarkSweepGC.h
@@ -12,56 +12,12 @@ public:
     size_t m_AllocsSinceGC = 0;
 
 public:
-	MarkSweepGC(const size_t gcInterval)
-		: m_GCInterval(gcInterval)
-	{
+    MarkSweepGC(const size_t gcInterval);
 
-	}
+    void* Allocate(const size_t size) override;
 
-    void* Allocate(const size_t size) override
-    {
-        if (++m_AllocsSinceGC == m_GCInterval)
-        {
-            CollectGarbage();
-            m_AllocsSinceGC = 0;
-        }
-        auto memory = ::operator new(size);
-        m_Allocated.insert(static_cast<Object*>(memory));
-        return memory;
-    }
+    void CollectGarbage();
 
-    void CollectGarbage()
-    {
-        m_Visited.clear();
-
-        // TODO:: what happens if  m_root == nullptr and m_AllocsSinceGC > 0 
-        if (m_Root == nullptr)
-        {
-            return;
-        }
-
-        m_Visited.insert(*m_Root);
-        (*m_Root)->VisitReferences(this, nullptr, m_Visited);
-        
-        for (const auto object : m_Allocated)
-        {
-            if (m_Visited.find(object) == m_Visited.end())
-            {
-                object->~Object();
-                ::operator delete(object);
-            }
-        }
-
-        m_Allocated = m_Visited;
-    }
-
-    void Shutdown() override
-    {
-        for (const auto object : m_Allocated)
-        {
-            object->~Object();
-            ::operator delete(object);
-        }
-    }
+    void Shutdown() override;
 };
 
